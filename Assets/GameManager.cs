8397b9d43d03b4552a2f05ddc54c04ce3b5401c7
chewang1 <kenclie@hotmail.com>
2018-08-30 09:57:52 +0800

    v1

---
+++ Assets/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isSpin = false;
+    public int probability = 10;
+    //the speed of reel
+    public float maxspeed = 100;
+    //user define how many times while spinning
+    public int spintimes = 0;
+
+    //2d array for saving values from numbers shown in panel
+    public int[,] values = new int[3,3];
+    public Text resultText1;
+    public Text resultText2;
+    public Text resultText3;
+    private bool isClicked = false;
+
+    public void onClickedSpin()
+    {
+        if (isSpin)
+        {
+            GetResult();
+            //show result
+            resultText1.text = values[0, 0].ToString() + " " + values[0, 1].ToString() + " " + values[0, 2].ToString();
+            resultText2.text = values[1, 0].ToString() + " " + values[1, 1].ToString() + " " + values[1, 2].ToString();
+            resultText3.text = values[2, 0].ToString() + " " + values[2, 1].ToString() + " " + values[2, 2].ToString();
+        }
+
+        //for (int i = 0; i < values.GetLength(0); i++)
+            //for (int j = 0; j < values.GetLength(1); j++)
+                //Debug.Log(values[i, j]);
+
+        isSpin = !isSpin;
+    }
+
+    public void GetResult()
+    {
+        //get result from 3 reels and save into 2d array
+        ReelSpin reel1 = this.transform.Find("Reel1").GetComponent<ReelSpin>();
+        ReelSpin reel2 = this.transform.Find("Reel2").GetComponent<ReelSpin>();
+        ReelSpin reel3 = this.transform.Find("Reel3").GetComponent<ReelSpin>();
+        for (int i = 0; i < values.GetLength(0); i++)
+        {
+            values[i, 0] = reel1.shownNumbers[i];
+            values[i, 1] = reel2.shownNumbers[i];
+            values[i, 2] = reel3.shownNumbers[i];
+        }
+    }
+
+    
+}
+
+public class GM
+{
+    private static GameManager m_gm = null;
+    public static GameManager GameManager
+    {
+        get
+        {
+            if (m_gm == null)
+                m_gm = GameObject.FindObjectOfType<GameManager>();
+            return m_gm;
+        }
+    }
+}
---
+++ Assets/ReelSpin.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReelSpin : MonoBehaviour
+{
+    
+    private float numDiff = 3;
+    //boundaries for numbers
+    private float lowerbound;
+    private float topbound;
+
+    //increase speed
+    private float curspeed = 0;
+    private float acceleration = 1;
+    [SerializeField]
+    public List<GameObject> numbers = new List<GameObject>();
+
+
+    public Sprite n1, n2, n3, n4, n5, n6, n7, n8, n9, n10;
+    private int curPtr;
+    public int[] slots;
+    public int[] shownNumbers;
+    //private GameObject number;
+    // Use this for initialization
+    void Start ()
+	{
+        //set lowerbound and topbound
+        lowerbound = numbers[numbers.Count - 1].transform.position.y;
+        topbound = numbers[0].transform.position.y + numDiff;
+
+        //init for random numbers in reel
+	    curPtr = Random.Range(0, numbers.Count);
+	    slots = new int[10];
+
+        //init for shown panel numbers
+	    shownNumbers = new int [3];
+        //set 1- 10 into slots
+        //set probability, 10 means 10 numbers in reel
+        setProbability(GM.GameManager.probability);
+	    
+        //do shffule
+        shuffle(slots);   
+
+        //assign to number objects
+	    assignNumber(slots);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //spin the reel
+	    if (GM.GameManager.isSpin)
+	    {
+	        foreach (GameObject number in numbers)
+	        {
+	            //moving downward and gradually increase speed
+	            number.transform.Translate(Vector3.down * curspeed * Time.deltaTime);
+	            curspeed += acceleration * Time.deltaTime;
+	            
+                Mathf.Clamp(curspeed, 0 ,GM.GameManager.maxspeed);
+                Vector3 pos = number.transform.position;
+
+	            if (number.transform.position.y < lowerbound)
+	            {
+	                number.transform.position = new Vector3(pos.x, topbound, pos.z);
+	                //float diff = numbers[9].transform.position.y - numbers[0].transform.position.y;
+	                //Debug.Log(diff);
+	            }
+	        }
+
+	        Spin();
+	        //GM.GameManager.GetResult();
+	    }
+	    else
+	    {
+            //stop spin with lower velocity
+	        foreach (GameObject number in numbers)
+	        {
+	            //curspeed = Mathf.Lerp(0, GM.GameManager.maxspeed, Time.deltaTime);
+                //number.transform.Translate(Vector3.down * curspeed * Time.deltaTime);
+                
+	        }
+	    }
+	}
+
+    void shuffle(int []slots)
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            
+            int r = Random.Range(i, slots.Length);
+            //swap
+            int temp = slots[i];
+            slots[i] = slots[r];
+            slots[r] = temp;
+
+        }
+    }
+
+    void assignNumber(int[] slots)
+    {
+        //change sprite pics
+        for (int i = 0; i < slots.Length; i++)
+        {
+            setSpritefromArray(numbers[i], slots[i]);
+        }
+    }
+
+    void setSpritefromArray(GameObject obj, int number)
+    {
+        switch(number)
+        {
+            case 1:
+                obj.GetComponent<SpriteRenderer>().sprite = n1;
+                break;
+            case 2:
+                obj.GetComponent<SpriteRenderer>().sprite = n2;
+                break;
+            case 3:
+                obj.GetComponent<SpriteRenderer>().sprite = n3;
+                break;
+            case 4:
+                obj.GetComponent<SpriteRenderer>().sprite = n4;
+                break;
+            case 5:
+                obj.GetComponent<SpriteRenderer>().sprite = n5;
+                break;
+            case 6:
+                obj.GetComponent<SpriteRenderer>().sprite = n6;
+                break;
+            case 7:
+                obj.GetComponent<SpriteRenderer>().sprite = n7;
+                break;
+            case 8:
+                obj.GetComponent<SpriteRenderer>().sprite = n8;
+                break;
+            case 9:
+                obj.GetComponent<SpriteRenderer>().sprite = n9;
+                break;
+            case 10:
+                obj.GetComponent<SpriteRenderer>().sprite = n10;
+                break;
+            default:
+                Debug.Log("no such number for sprite");
+                break;
+        }
+    }
+
+    //function for set random number range into slots array
+    void setProbability(int numberRange)
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            slots[i] = (i + 1) % (numberRange + 1);
+            if (slots[i] == 0)
+                slots[i] = Random.Range(1, numberRange);
+        }
+            
+    }
+
+    //function for get result numbers
+    void Spin()
+    {
+        int randspin = Random.Range(0, GM.GameManager.spintimes);
+
+        for (int i = 0; i < randspin; i++)
+        {
+            curPtr++;
+            if (curPtr > 8)
+                curPtr = 0;
+        }
+
+        int prev = curPtr - 1;
+        int next = curPtr + 1;
+
+        if (prev < 0)
+            prev = 8;
+        if (next > 8)
+            next = 0;
+
+        shownNumbers[0] = slots[prev];
+        shownNumbers[1] = slots[curPtr];
+        shownNumbers[2] = slots[next];
+
+        //for(int i=0; i < shownNumbers.Length;i++)
+            //Debug.Log(shownNumbers[i]);
+    }
+
+
+}
